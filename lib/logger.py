7b6803c70ab1308c64eaf060d859aaa6cd59a9ae
ROZ a.k.a. Aoi Emerauda <mail@mofumofu.me>
2024-03-27 17:45:05 +0900

    Various Cleanups

--- lib/logger.py
+++ lib/logger.py
@@ -7,24 +7,6 @@ from twisted.python import log as twisted_log
 
 import settings
 
-'''
-class Logger(object):
-    def debug(self, msg):
-        twisted_log.msg(msg)
-
-    def info(self, msg):
-        twisted_log.msg(msg)
-
-    def warning(self, msg):
-        twisted_log.msg(msg)
-        
-    def error(self, msg):
-        twisted_log.msg(msg)
-        
-    def critical(self, msg):
-        twisted_log.msg(msg)
-'''
-
 def get_logger(name):    
     logger = logging.getLogger(name)
     logger.addHandler(stream_handler)
@@ -33,9 +15,7 @@ def get_logger(name):
     if settings.LOGFILE != None:
         logger.addHandler(file_handler)
     
-    logger.debug("Logging initialized")
     return logger
-    #return Logger()
 
 if settings.DEBUG:
     fmt = logging.Formatter("%(asctime)s %(levelname)s %(name)s %(module)s.%(funcName)s # %(message)s")
